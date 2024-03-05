5a2f2016b2b53cc33010009719b9c81e2b95db03
davidlehner1 <d.lehner2@students.htl-leonding.ac.at>
2024-03-05 12:19:42 +0100

    -overworked

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 mvn -B package
 cp src/main/docker/Dockerfile target/
-docker login ghcr.io -u $GITHUB_ACTOR -p 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
+docker login ghcr.io -u $GITHUB_ACTOR -p $GITHUB_TOKEN
 docker build --tag ghcr.io/davidlehner1/backend:latest ./target
 docker push ghcr.io/davidlehner1/backend:latest
